f23112210fd69187212fc44fa5e58e15b4e9f24e
Alfatio001 <151828772+Alfatio001@users.noreply.github.com>
2024-04-29 19:55:20 +0700

    Add files via upload

---
+++ no2.py
@@ -0,0 +1,8 @@
+def main():
+  n = int(input("Masukkan sebuah bilangan: "))
+  if n % 2 == 0:
+    print("Bilangan {} adalah genap.".format(n))
+  else:
+    print("Bilangan {} adalah ganjil.".format(n))
+
+main()
